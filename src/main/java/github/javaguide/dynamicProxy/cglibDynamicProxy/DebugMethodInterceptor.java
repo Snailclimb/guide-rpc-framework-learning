f02646693daa55e692f6c6bd51d4b452e92599b1
guide <koushuangbwcx@163.com>
2020-07-25 15:23:15 +0800

    [refractor]

--- src/main/java/github/javaguide/dynamicProxy/cglibDynamicProxy/DebugMethodInterceptor.java
+++ src/main/java/github/javaguide/dynamicProxy/cglibDynamicProxy/DebugMethodInterceptor.java
@@ -8,23 +8,20 @@ import java.lang.reflect.Method;
 /**
  * 自定义MethodInterceptor
  */
-public class DebugMethodInterceptor implements MethodInterceptor{
+public class DebugMethodInterceptor implements MethodInterceptor {
 
 
     /**
-     *
-     * @param o cglib生成的代理对象
-     * @param method 被代理对象方法
-     * @param objects 方法入参
-     * @param methodProxy 代理方法
-     * @return
-     * @throws Throwable
+     * @param o           被代理的对象（需要增强的对象）
+     * @param method      被拦截的方法（需要增强的方法）
+     * @param args        方法入参
+     * @param methodProxy 用于调用原始方法
      */
     @Override
-    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+    public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
         //调用方法之前，我们可以添加自己的操作
         System.out.println("before method " + method.getName());
-        Object object = methodProxy.invokeSuper(o, objects);
+        Object object = methodProxy.invokeSuper(o, args);
         //调用方法之后，我们同样可以添加自己的操作
         System.out.println("after method " + method.getName());
         return object;
